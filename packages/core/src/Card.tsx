b7d5796d1d0e5f2c1cd5a5bbe20e6896b813f5f4
Saiki K <pacexy@gmail.com>
2022-03-30 21:35:09 +0800

    feat(docs): `<Card>`

--- packages/core/src/Card.tsx
+++ packages/core/src/Card.tsx
@@ -1,24 +1,34 @@
 import clsx from 'clsx'
 import { ComponentPropsWithoutRef } from 'react'
+import { objectKeys } from 'ts-extras'
+
+import { StateLayer } from './StateLayer'
+import { classes } from './classes'
 
 const variantMap = {
   elevated: 'bg-surface shadow-1',
   filled: 'bg-surface-variant',
-  outlined: 'bg-surface border border-outline',
+  outlined: clsx('bg-surface', classes.outlined),
 }
 
 export interface CardProps extends ComponentPropsWithoutRef<'div'> {
   variant: keyof typeof variantMap
 }
-export const Card: React.FC<CardProps> = ({ className, variant, ...props }) => {
+export function Card({ className, variant, children, ...props }: CardProps) {
+  const interactive = !!props.onClick
   return (
     <div
       className={clsx(
-        'bg-surface1 rounded-xl p-4',
+        'rounded-xl p-4',
         variantMap[variant],
+        interactive && 'relative cursor-pointer overflow-hidden',
         className,
       )}
       {...props}
-    />
+    >
+      {interactive && <StateLayer />}
+      {children}
+    </div>
   )
 }
+Card.variants = objectKeys(variantMap)
